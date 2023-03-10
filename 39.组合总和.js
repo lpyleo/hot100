95418dd90e50d395814034ce52b0a083db8664b2
lpy <335896431@qq.com>
2023-03-10 22:51:21 +0800

    Complete 39

--- 39.组合总和.js
+++ 39.组合总和.js
@@ -11,7 +11,24 @@
  * @return {number[][]}
  */
 var combinationSum = function(candidates, target) {
-
+    candidates = candidates.sort((a, b) => (a-b))
+    let path = [] // 路径
+    let result = [] //返回的结果
+    function backTrack(start, sum){
+        if(sum === target){
+            result.push([...path])
+        }
+        for(let i = start;i<candidates.length;i++){
+            if(sum+candidates[i]>target){
+                return
+            }
+            path.push(candidates[i])
+            backTrack(i,sum+candidates[i])
+            path.pop()
+        }
+    }
+    backTrack(0,0)
+    return result
 };
 // @lc code=end
 
