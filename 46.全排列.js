2b9bb9d8a055c5fa2ba3c234273706143847f9ad
lpy <335896431@qq.com>
2023-03-10 23:31:01 +0800

    complate 46

--- 46.全排列.js
+++ 46.全排列.js
@@ -10,7 +10,24 @@
  * @return {number[][]}
  */
 var permute = function(nums) {
-    
+    let result = []
+    backTrack(nums, result, [])
+    return result
 };
+
+function backTrack(nums, result, temp){
+    if(nums.length === temp.length){
+        result.push([...temp])
+        return
+    }
+    for(let i = 0;i<nums.length;i++){
+        if(temp.includes(nums[i])){
+            continue
+        }
+        temp.push(nums[i])
+        backTrack(nums, result, temp)
+        temp.pop()
+    }
+}
 // @lc code=end
 
